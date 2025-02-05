eb7d12e6281af74f53f8c67e8f0ecd2d127a31d2
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-02-06 00:05:23 +0400

    new

---
+++ assets/js/swiper.js
@@ -0,0 +1,88 @@
+
+
+
+let advantagesSwiper = new Swiper(".transform-swiper", {
+    spaceBetween: 21,
+    slidesPerView: 3,
+    loop: true,
+
+    pagination: {
+        el: ".transform-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        320: {slidesPerView: 1, spaceBetween: 10,},
+        400: {slidesPerView: 1, spaceBetween: 10,},
+        540: {slidesPerView: 1.3,},
+        576: {slidesPerView: 1.5},
+        779: {slidesPerView: 2},
+        954: {slidesPerView: 2.5},
+        1139: {slidesPerView: 3},
+        1200: {slidesPerView: 3},
+
+    },
+
+
+});
+
+
+
+function initSwiper() {
+    let reviewsSwiper;
+
+    const isMobile = window.innerWidth <= 992;
+    const direction = isMobile ? "horizontal" : "vertical";
+
+    if (reviewsSwiper) {
+        reviewsSwiper.destroy(true, true);
+    }
+
+    reviewsSwiper = new Swiper(".reviews-swiper", {
+        direction: direction,
+        spaceBetween: 16,
+        slidesPerView: 3,
+        pagination: {
+            el: ".reviews-pagination",
+            clickable: true,
+        },
+        loop: true,
+        autoplay: {
+            delay: 10,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+
+        breakpoints: {
+            320: {slidesPerView: 1,},
+            490: {slidesPerView: 1.2,},
+            620: {slidesPerView: 1.5,},
+            700: {slidesPerView: 1.5},
+            897: {slidesPerView: 2},
+            992: {slidesPerView: 2.6},
+            1096: {slidesPerView: 3},
+            1232: {slidesPerView: 3},
+            1920: {slidesPerView: 3},
+        },
+
+    });
+    const swiperContainer = document.querySelector(".reviews-swiper");
+
+    if(swiperContainer){
+
+        swiperContainer.addEventListener("mouseenter", () => {
+            reviewsSwiper.autoplay.stop();
+        });
+
+        swiperContainer.addEventListener("mouseleave", () => {
+            reviewsSwiper.autoplay.start();
+        });
+    }
+
+}
+
+initSwiper();
+
